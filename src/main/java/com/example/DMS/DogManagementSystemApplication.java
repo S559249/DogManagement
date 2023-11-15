3682f06d363563001b8826b0dad5732b5fb07d2d
S559249 <123011251+S559249@users.noreply.github.com>
2023-11-14 21:36:52 -0600

    first commit

---
+++ src/main/java/com/example/DMS/DogManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package com.example.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+*
+* @author Aasrita Emani
+*/
+@SpringBootApplication
+public class DogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,14 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+/**
+*
+* @author Aasrita Emani
+*/
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
